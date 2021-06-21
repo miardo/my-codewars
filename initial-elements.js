49ea87da8ec92c7b457dd85ef5bd61daff5e58fa
KARIMOV <marat.karimov@icloud.com>
2021-06-22 00:14:33 +0400

    Исправляем стили

--- initial-elements.js
+++ initial-elements.js
@@ -158,5 +158,24 @@ export const initialElements = [
             })
                 return newA
                 }`
+    },
+    {
+        kata: '8',
+        task: `You get an array of numbers, return the sum of all of the positives ones.
+
+                Example [1,-4,7,12] => 1 + 7 + 12 = 20
+                
+                Note: if there is nothing to sum, the sum is default to 0.`,
+        code: `function positiveSum(arr) {
+                    let summ = 0;
+                    for (let i = 0; i < arr.length; i++) {
+                    if (arr[i] > 0) {
+                        summ += arr [i]
+                    } else {
+                        summ += 0
+                    }
+                    }
+                    return summ
+                }`
     }
 ];
