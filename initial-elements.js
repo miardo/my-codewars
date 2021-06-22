7f9e101e11fc235b04e1e98be75641018a79c47a
KARIMOV <marat.karimov@icloud.com>
2021-06-22 20:41:46 +0400

    Добавляем сделанное задание

--- initial-elements.js
+++ initial-elements.js
@@ -177,5 +177,33 @@ export const initialElements = [
                     }
                     return summ
                 }`
+    },
+    {
+        kata: '7',
+        task: `Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1 (in C#, N might be less then 1).
+
+                Replace certain values however if any of the following conditions are met:
+                
+                If the value is a multiple of 3: use the value "Fizz" instead
+                If the value is a multiple of 5: use the value "Buzz" instead
+                If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+                Method calling example:
+                
+                fizzbuzz(3) -->  [1, 2, "Fizz"]`,
+        code: `function fizzbuzz(n) {
+            let array = [];
+            for (let i = 1; i <= n; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                array.push('FizzBuzz')
+            } else if (i % 5 === 0) {
+                array.push('Buzz')
+            } else if (i % 3 === 0) {
+                array.push('Fizz')
+            } else {
+                array.push(i)
+            }
+            }
+            return array
+        }`
     }
 ];
