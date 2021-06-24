4779f07b9fecdc4999b4e9333084ecf17b36b303
KARIMOV <marat.karimov@icloud.com>
2021-06-24 23:28:58 +0400

    Добавляем задачку

--- initial-elements.js
+++ initial-elements.js
@@ -219,5 +219,22 @@ export const initialElements = [
             }
         return Math.min.apply(null, newArr)
         }`
+    },
+    {
+        kata: '7',
+        task: `Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+        For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+        
+        Note: The function accepts an integer and returns an integer`,
+        code: `function squareDigits(num){
+            const numString = String(num);
+            const numArray = numString.split('');
+            let newNum = [];
+            for (let i = 0; i < numArray.length; i++) {
+               newNum.push(numArray[i] * numArray[i])
+            }
+            return Number(newNum.join(''))
+            }`
     }
 ];
