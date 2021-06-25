b83901c1888e5d8248d59f44b5c5b46900e960cb
KARIMOV <marat.karimov@icloud.com>
2021-06-25 22:27:51 +0400

    Добавляем задание

--- initial-elements.js
+++ initial-elements.js
@@ -236,5 +236,29 @@ export const initialElements = [
             }
             return Number(newNum.join(''))
             }`
+    },
+    {
+        kata: '7',
+        task: `The code provided is supposed return a person's Full Name given their first and last names.
+
+            But it's not working properly.
+            
+            Notes
+            The first and/or last names are never null, but may be empty.
+            
+            Task
+            Fix the bug so we can all go home early.`,
+        code: `class Dinglemouse{
+
+                constructor( firstName, lastName ){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                }
+                
+                getFullName(){
+                return (this.firstName +" "+ this.lastName).trim()
+                }
+                
+            }`
     }
 ];
