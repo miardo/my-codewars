e059e55d222ffbe8853775eb40bc9375de13f28c
KARIMOV <marat.karimov@icloud.com>
2021-07-08 22:26:40 +0400

    feat: add new task

--- initial-elements.js
+++ initial-elements.js
@@ -365,5 +365,29 @@ export const initialElements = [
         code: `function litres(time) {
             return Math.floor(time * 0.5);
         }`
+    },
+    {
+        kata: '7',
+        task: `You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+        Complete the method which accepts such an array, and returns that single different number.
+        
+        The input array will always be valid! (odd-length >= 3)
+        
+        Examples
+        [1, 1, 2] ==> 2
+        [17, 17, 3, 17, 17, 17, 17] ==> 3`,
+        code: `function stray(numbers) {
+            let num = 0;
+            if (numbers[0] === numbers[1]) {
+            num = numbers[0];
+            } else if (numbers[0] != numbers[1]) {
+            num = numbers[2];
+            }
+            for (let i = 0; i <= numbers.length; i++) {
+            if (numbers[i] != num)
+                return numbers[i]
+            }
+        }`
     }
 ];
