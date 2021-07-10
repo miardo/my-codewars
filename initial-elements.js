6376038c86daf2cf8baaac5bb8e19ab0c3fac57d
KARIMOV <marat.karimov@icloud.com>
2021-07-10 20:30:04 +0400

    feat: add new task

--- initial-elements.js
+++ initial-elements.js
@@ -389,5 +389,39 @@ export const initialElements = [
                 return numbers[i]
             }
         }`
+    },
+    {
+        kata: '7',
+        task: `Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
+
+        Example:
+        
+        Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+        
+        Addendum:
+        
+        Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+        
+        The resultant arrays should be in the order they were originally in and should not have its values changed.
+        
+        No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.`,
+        code: `function filterHomogenous(arrays) {
+            let arr = [];
+            for (let i = 0; i < arrays.length; i++) {
+                let count = 0;
+            const newArr = arrays[i];
+            for (let j = 0; j < newArr.length; j++) {
+                if (typeof newArr[0] === typeof newArr[j] || newArr.length === 1) {
+                count++
+                } else {
+                count = 0
+                }
+            }
+            if (count === newArr.length && newArr.length != 0) {
+                arr.push(arrays[i])
+            }
+            }
+            return arr;
+        }`
     }
 ];
