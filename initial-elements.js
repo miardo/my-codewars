ed57d6854870a2d6618458065380e32ef912cd5d
KARIMOV <marat.karimov@icloud.com>
2021-07-11 12:46:11 +0400

    feat: add new task

--- initial-elements.js
+++ initial-elements.js
@@ -423,5 +423,51 @@ export const initialElements = [
             }
             return arr;
         }`
+    },
+    {
+        kata: '7',
+        task: `You have a group chat application, but who is online!?
+
+        You want to show your users which of their friends are online and available to chat!
+        
+        Given an input of an array of objects containing usernames, status and time since last activity (in mins), create a function to work out who is online, offline and away.
+        
+        If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
+        
+        If for example, no users are online the output should look as follows:
+
+{
+  offline: ['Lucy'],
+  away: ['Bob']
+}
+username will always be a string, status will always be either 'online' or 'offline' (UserStatus enum in C#) and lastActivity will always be number >= 0.
+
+Finally, if you have no friends in your chat application, the input will be an empty array []. In this case you should return an empty object {} (empty Dictionary in C#).`,
+        code: `const whosOnline = (friends) => {
+            let nameOnline = [];
+            let nameOffline = [];
+            let nameAway = [];
+            let newObj = {
+            }
+            for (let i = 0; i < friends.length; i++) {
+            if (friends[i].status === 'online' && friends[i].lastActivity <= 10) {
+            nameOnline.push(friends[i].username)
+            } else if (friends[i].status === 'offline' ) {
+                nameOffline.push(friends[i].username)
+            } else if (friends[i].status === 'online' && friends[i].lastActivity > 10) {
+            nameAway.push(friends[i].username)
+            }
+            }
+                if (nameOnline.length > 0) {
+            newObj.online = nameOnline
+            } 
+            if (nameOffline.length > 0) {
+            newObj.offline = nameOffline
+            } 
+            if (nameAway.length > 0) {
+            newObj.away = nameAway
+            }
+            return newObj
+        }`
     }
 ];
